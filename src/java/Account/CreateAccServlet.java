9c1675fcf90330e3d3ff51ed944057c71517898a
ReedS <ReedStock1992@gmail.com>
2018-04-10 23:40:36 -0500

    - Create account working (updating database)
    - To Do:
    
    1. Sessions
    2. Email
    3. Forgot Password

--- src/java/Account/CreateAccServlet.java
+++ src/java/Account/CreateAccServlet.java
@@ -8,6 +8,7 @@ package Account;
 
 
 import java.io.*;
+import static java.lang.System.out;
 import javax.servlet.http.*;
 import javax.servlet.*;
 import java.sql.*;
@@ -15,42 +16,42 @@ import java.sql.*;
 
 public class CreateAccServlet extends HttpServlet {
 
-        @Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-                // Request info from html form
-                String email=request.getParameter("email");
-		String password=request.getParameter("password");
-                String fname=request.getParameter("fname");
-                String lname=request.getParameter("lname");
-                String zip=request.getParameter("zip");
-		
-		// Connect to mysql and verify username password
-		
-		try {
-		Class.forName("com.mysql.jdbc.Driver");
-		// loads driver
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/weatherdb", "root", ""); // gets a new connection
- 
-		PreparedStatement ps = c.prepareStatement("Insert into login (email,password,fname,lname,zip) values(?,?,?,?,?)");
-		ps.setString(1, email);
-		ps.setString(2, password);
-                ps.setString(3, fname);
-                ps.setString(4, lname);
-                ps.setString(5, zip);
- 
-		ResultSet rs = ps.executeQuery();
- 
-		while (rs.next()) {
-			response.sendRedirect("index.html");
-			return;
-		}
-		response.sendRedirect("error.html");
-		return;
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-            
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int numRowsChanged = 0;
+        
+        // Request info from html form
+        String email=request.getParameter("email");
+        String password=request.getParameter("password");
+        String fname=request.getParameter("fname");
+        String lname=request.getParameter("lname");
+        String zip=request.getParameter("zip");
+
+        // Connect to mysql and verify username password
+
+        try {
+        Class.forName("com.mysql.jdbc.Driver");
+        // loads driver
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/weatherdb", "root", ""); // gets a new connection
+
+        PreparedStatement ps = c.prepareStatement("insert into login values (?,?,?,?,?)");
+        ps.setString(1, email);
+        ps.setString(2, password);
+        ps.setString(3, fname);
+        ps.setString(4, lname);
+        ps.setString(5, zip);
+        
+        int i=ps.executeUpdate();  
+        if(i>0)  {
+        out.print("You are successfully registered...");  
+        response.sendRedirect("index.html");
+        }
+        
+        }catch (Exception e2) {
+            out.println(e2);  
+            //response.sendRedirect("createaccount.html");
+            }
+        out.close(); 
+    } 
 }
