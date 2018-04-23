e7e73c8c5eb02e6fbb1959ae47aed61e8cf99892
ReedS <ReedStock1992@gmail.com>
2018-04-22 22:13:31 -0500

    - Initial commit

--- src/java/Account/LoginServlet.java
+++ src/java/Account/LoginServlet.java
@@ -31,17 +31,17 @@ public class LoginServlet extends HttpServlet {
             Class.forName("com.mysql.jdbc.Driver");
             // loads driver
             Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/weatherdb", "root", ""); // gets a new connection
-            PreparedStatement ps = c.prepareStatement("Select email, password, type from login where email=? and password=?");
+            PreparedStatement ps = c.prepareStatement("Select email, password from login where email=? and password=?");
             ps.setString(1, email);
             ps.setString(2, password);
-            ps.setString(3,type);
+            //ps.setString(3,type);
             
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
                     HttpSession session = request.getSession();
                     session.setAttribute("email", email);
-                    session.setAttribute("type", type);
+                    //session.setAttribute("type", type);
  
                     response.sendRedirect("index.jsp");
                     return;
